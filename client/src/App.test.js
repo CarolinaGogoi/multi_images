5b60c16dd720a1fb9a83f94e31f7c7ae7e6366c9
J Gogoi <codergogoi@gmail.com>
2020-12-19 09:42:29 +0530

    Case 1

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+ 
 
-it('renders correctly', async () => {
-  renderer.create(<App />);
+test('renders learn react link', async() => {
+  render(<App />);
+  const linkElement = screen.getByText(/Welcome to my App/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Welcome to my App/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
